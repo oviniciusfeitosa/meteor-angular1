a518b9e773f4eb8d191eca30e863290a62636f44
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2016-09-30 15:35:07 -0300

    [update] Fim dos passos de 'Step 4'

--- imports/ui/components/partiesList/partiesList.js
+++ imports/ui/components/partiesList/partiesList.js
@@ -4,6 +4,7 @@ import angularMeteor from 'angular-meteor';
 import template from './partiesList.html';
 import { Parties } from '../../../api/parties';
 import { name as PartyAdd } from '../partyAdd/partyAdd';
+import { name as PartyRemove } from '../partyRemove/partyRemove';
 
 // Agora está em ES6 - EcmaScript 6 e sendo carregado dinamicamente via LazyLoad
 class PartiesList {
@@ -25,7 +26,8 @@ const name = 'partiesList';
 // create a module
 export default angular.module(name, [
   angularMeteor,
-  PartyAdd
+  PartyAdd,
+  PartyRemove
 ]).component(name, {
   template,
   controllerAs: name,
---
+++ imports/ui/components/partyRemove/partyRemove.js
@@ -0,0 +1,28 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+
+import template from './partyRemove.html';
+import { Parties } from '../../../api/parties';
+
+class PartyRemove {
+	remove() {
+		if(this.party) {
+			console.log(this.party);
+			Parties.remove(this.party._id);
+		}
+	}
+}
+
+const name = 'partyRemove';
+
+//create a module
+export default angular.module(name, [
+	angularMeteor
+]).component(name, {
+	template,
+	binding: {
+		party: '<'
+	},
+	controllerAs: name,
+	controller: PartyRemove
+});
