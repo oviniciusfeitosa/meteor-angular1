0f937c21c259e69f6a045c5e13fbf8de2660ff8f
vinnyfs89 <viniciusfesil@gmail.com>
2016-10-04 02:31:51 -0300

    [update] fim do tópico template e inicio do tópico Routes definition

--- imports/ui/components/socially/socially.js
+++ imports/ui/components/socially/socially.js
@@ -1,8 +1,10 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
  
 import template from './socially.html';
 import { name as PartiesList } from '../partiesList/partiesList';
+import { name as Navigation } from '../navigation/navigation';
  
 class Socially {}
  
@@ -11,9 +13,20 @@ const name = 'socially';
 // create a module
 export default angular.module(name, [
   angularMeteor,
-  PartiesList
+  uiRouter,
+  PartiesList,
+  Navigation
 ]).component(name, {
   template,
   controllerAs: name,
   controller: Socially
-});
+})
+.config(config);
+
+function config($locationProvider, $urlRouterProvider) {
+	'ngInject';
+ 
+	$locationProvider.html5Mode(true);
+
+	$urlRouterProvider.otherwise('/parties');
+}
