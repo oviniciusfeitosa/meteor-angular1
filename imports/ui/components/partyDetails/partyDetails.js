a477c07954685faa79735e77bf14a66a3a6b4494
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2016-10-13 16:46:10 -0300

    [ add / update ] Conclusão do Step 6 - Atualização de um item ao clicar em seu detalhamento

--- imports/ui/components/partyDetails/partyDetails.js
+++ imports/ui/components/partyDetails/partyDetails.js
@@ -1,13 +1,41 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
-import uiRouter from 'angular-ui-router';
 
 import template from './partyDetails.html';
+import { Parties } from '../../../api/parties';
 
 class PartyDetails {
-    constructor($stateParams) {
+    constructor($stateParams, $scope, $reactive) {
         'ngInject';
+
+        $reactive(this).attach($scope);
+
         this.partyId = $stateParams.partyId;
+
+        this.helpers({
+            party() {
+                return Parties.findOne({
+                    _id: $stateParams.partyId
+                });
+            }
+        });
+    }
+
+    save() {
+        Parties.update({
+            _id: this.party._id
+        }, {
+            $set: {
+                name: this.party.name,
+                description: this.party.description
+            }
+        }, (error) => {
+            if (error) {
+                console.log('Oops, unable to update the party...');
+            } else {
+                console.log('Done!');
+            }
+        });
     }
 }
 
